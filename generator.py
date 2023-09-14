4a743d46a51c25db45de3e98e3589caedc9b48f9
mkhripkov <michaelkhripkov@gmail.com>
2023-09-14 16:54:54 -0400

    Added GUI

--- generator.py
+++ generator.py
@@ -1,12 +1,34 @@
 import secrets # Using secrets to generate the password rather than random is more secure
+import PySimpleGUI as sg
 
-possible_characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%^&*()?'
+# GUI 
+sg.theme('DarkPurple4')
 
-password_length = int(input('How long would you like your password to be?'))
+layout = [
+    [sg.Text('Enter Desired Password Length:')],
+    [sg.Input(key = '-INPUT-', size=5)],
+    [sg.Button('Generate', key='-BUTTON1-')],
+    [sg.Text('Password:'), sg.Multiline(size = 35, no_scrollbar=True, disabled=True, key='-PASSWORD-')]    
+    ]
 
-password = ''
+window = sg.Window('Python Password Generator', layout, size=(300, 100), resizable=True)
 
-for x in range(password_length):
-    password += secrets.choice(possible_characters) # secrets randomly selects a character and adds it to the password string
+while True:
+    event, values = window.read()
     
-print(f'Your password is: {password}')
+    if event == sg.WIN_CLOSED: # Closes window when X button is clicked
+        break 
+    
+    if event == '-BUTTON1-':
+        try:
+            # Password Generation
+            possible_characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%^&*()?'
+            password_length = int(values['-INPUT-'])
+            password = ''
+            for x in range(password_length):
+                password += secrets.choice(possible_characters) # secrets randomly selects a character and adds it to the password string
+            window['-PASSWORD-'].update(password)
+        except ValueError:
+            window['-PASSWORD-'].update('Invalid Input')
+    
+window.close()
